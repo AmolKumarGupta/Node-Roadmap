c308f4fd68b859586f72fee26ce0dfa7193aff44
amol <amlo1232001@gmail.com>
2023-04-23 00:56:47 +0530

    feat: setup

--- express-auth/server.js
+++ express-auth/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("node:path");
 const { default: mongoose } = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("req-flash");
 const { uri } = require("./dbconfig");
-const User = require("./models/User");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -12,6 +14,15 @@ app.set("views", "views");
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "30dayofnode",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: uri }),
+  })
+);
+app.use(flash());
 
 app.use((req, res) => {
   res.render("errors/404");
